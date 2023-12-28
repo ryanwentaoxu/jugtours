a176cfeda988c49812df88c5cc2df6648c87d95f
ryanwentaoxu <wx225@cornell.edu>
2023-12-28 02:10:05 -0500

    feature group

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 practice
+
+jdk-17
+springboot 3.2.1
--- src/main/java/com/okta/developer/jugtours/model/Group.java
+++ src/main/java/com/okta/developer/jugtours/model/Group.java
@@ -14,5 +14,19 @@ import java.util.Set;
 @Entity
 @Table(name = "user_group")
 public class Group {
-  
+  @Id
+  @GeneratedValue
+  private Long id;
+  @NonNull
+  private String name;
+  private String address;
+  private String city;
+  private String stateOrProvince;
+  private String country;
+  private String postalCode;
+  @ManyToOne(cascade=CascadeType.PERSIST)
+  private User user;
+
+  @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+  private Set<Event> events;
 }
